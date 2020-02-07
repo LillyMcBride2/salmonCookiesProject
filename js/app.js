7033c163df5db4c7f993e11105a02465d8a4d810
lilly <lillianvioletmcbride@gmail.com>
2020-02-06 23:39:49 -0600

    Setup/deadline

---
+++ js/app.js
@@ -0,0 +1,45 @@
+
+var seattle = {
+    minHourlyCustomers: 23,
+    maxHourlyCustomers: 65,
+    avgCookiesPerCustomer: 6.3,
+    randomCustomersPerHour: function () {
+        Math.floor.random()*(maxHourlyCustomers - minHourlyCustomers) 
+        + minHourlyCustomers;
+    },
+}
+
+var tokyo = {
+    minHourlyCustomers: 3,
+    maxHourlyCustomers: 24,
+    avgCookiesPerCustomer: 1.2,
+};
+
+var dubai  = {
+    minHourlyCustomers: 11,
+    maxHourlyCustomers: 38,
+    avgCookiesPerCustomer: 3.7,
+}
+
+var paris = {
+    minHourlyCustomers: 20,
+    maxHourlyCustomers: 38,
+    avgCookiesPerCustomer: 2.3,
+}
+
+    var lima = {
+    minHourlyCustomers: 2,
+    maxHourlyCustomers: 16,
+    avgCookiesPerCustomer: 4.6,
+}
+function setHourlyCustomers () {
+
+    var times = [openingTime, closingTime]
+    while (openingTime < closingTime)
+    Math.random()
+
+
+}
+
+
+
