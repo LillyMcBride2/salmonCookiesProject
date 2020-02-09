684f68c799b5b9c70a00b56162d0b797fa3f61e3
lilly <lillianvioletmcbride@gmail.com>
2020-02-08 18:19:31 -0600

    beginning work on constructor

--- js/app.js
+++ js/app.js
@@ -1,38 +1,51 @@
-var hourlyTimeArray = ['7AM:', '8AM:', '9AM:', '10AM:', '11AM:', 'NOON:', '1PM:', '2PM:', '3PM:', '4PM:', '5PM:', '6PM:', '7PM:', '8PM:', '9PM:']
-function setHourlyCustomers(min, max){
-    var answer =  Math.floor((Math.random())*(max-min))+min;
-    return answer;
+'use strict';
+var hourlyTimeArray = ['7AM:', '8AM:', '9AM:', '10AM:', '11AM:', 'NOON:', '1PM:', '2PM:', '3PM:', '4PM:', '5PM:', '6PM:', '7PM:', '8PM:', '9PM:'];
+
+function Location(min, max, avg){
+    this.minHourlyCustomers = min;
+    this.maxHourlyCustomers = max;
+    this.avgCookiesPerCustomer = avg;
+    this.hourlyCustomerArray = this.setHourlyCustomerArray;
+    this.hourlyCookieArray = this.setHourlyCookieArray;
+
 }
-function makeUL(array) {
-    var list = document.createElement('ul');
-    for (var i = 0; i < hourlyTimeArray.length; i++) {
-        var item = document.createElement('li');
-        item.appendChild(document.createTextNode(hourlyTimeArray[i] + ' ' + array[i] + ' cookies'));
-        list.appendChild(item);
+
+    Location.prototype.makeUL = function(array) {
+        var list = document.createElement('ul');
+        for (var i = 0; i < hourlyTimeArray.length; i++) {
+            var item = document.createElement('li');
+            item.appendChild(document.createTextNode(hourlyTimeArray[i] + ' ' + array[i] + ' cookies'));
+            list.appendChild(item);
+            }
+            return list;
         }
-        return list;
+
+    Location.prototype.setHourlyCustomers = function(min, max){
+        var answer =  Math.floor((Math.random())*(max-min))+min;
+        return answer;
     }
 
-var seattle = {
-    minHourlyCustomers: 23,
-    maxHourlyCustomers: 65,
-    avgCookiesPerCustomer: 6.3,
-    hourlyCustomerArray: [],
-    hourlyCookieArray:[],
-    setHourlyCustomerArray: function () {
+    Location.prototype.setHourlyCustomerArray = function() {
+   
         var hourlyCustomers = Math.floor(setHourlyCustomers(this.minHourlyCustomers, this.maxHourlyCustomers));
     
         for (i=0; i< 15; i++) {
-            this.hourlyCustomerArray[i] = hourlyCustomers;
-            hourlyCustomers = Math.floor(setHourlyCustomers(this.minHourlyCustomers, this.maxHourlyCustomers));
-        }
+             this.hourlyCustomerArray[i] = hourlyCustomers;
+             hourlyCustomers = Math.floor(setHourlyCustomers(this.minHourlyCustomers, this.maxHourlyCustomers));
+         }
+    }
+    Location.prototype.setHourlyCookieArray = function() {
         for (i=0; i< 15; i++) {
             this.hourlyCookieArray[i] = Math.floor((this.avgCookiesPerCustomer * this.hourlyCustomerArray[i]));
         }
+
     }
-}
-    seattle.setHourlyCustomerArray();
-    document.getElementById("seattle").appendChild(makeUL(seattle.hourlyCookieArray));
+        
+
+
+
+ seattle.setHourlyCustomerArray();
+ document.getElementById("seattle").appendChild(makeUL(seattle.hourlyCookieArray));
 
 
 
